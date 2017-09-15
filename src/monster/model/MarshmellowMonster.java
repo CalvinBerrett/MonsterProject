d4e39c5e79c4334dfb3d334ac036ee70a9b7b59e
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-09-15 12:12:01 -0600

    Added to String method to describe my monster

--- src/monster/model/MarshmellowMonster.java
+++ src/monster/model/MarshmellowMonster.java
@@ -2,5 +2,32 @@ package monster.model;
 
 public class MarshmellowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmellowMonster()
+	{
+		//initializes everything to 0, false or null.
+	}
+	
+	public MarshmellowMonster(String name, int eyeCount, int armCount, int tentacleAmount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = armCount;
+		this.tentacleAmount = tentacleAmount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only ";
+		description += tentacleAmount + " tentacles, but I have " + armCount + " arms!" + hasBloop + " tells you";
+		description += " if I have a bloop";
+		return description;
+	}
 }
