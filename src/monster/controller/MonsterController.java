6d790081e6ecd22a252d53f025bd8c7e50803703
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-09-15 12:54:23 -0600

    Added getters and setters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,15 @@
 package monster.controller;
 
+import monster.model.MarshmellowMonster;
+
 public class MonsterController
 {
-
+	
+	public void start()
+	{
+		MarshmellowMonster basic = new MarshmellowMonster();
+		System.out.println(basic);
+		MarshmellowMonster jeremy = new MarshmellowMonster("Silly Jermemy Monster", 7, 3, 6.7, true);
+		System.out.println(jeremy);
+	}
 }
--- src/monster/model/MarshmellowMonster.java
+++ src/monster/model/MarshmellowMonster.java
@@ -15,7 +15,7 @@ public class MarshmellowMonster
 		//initializes everything to 0, false or null.
 	}
 	
-	public MarshmellowMonster(String name, int eyeCount, int armCount, int tentacleAmount, boolean hasBloop)
+	public MarshmellowMonster(String name, int eyeCount, int armCount, double tentacleAmount, boolean hasBloop)
 	{
 		this.name = name;
 		this.eyeCount = armCount;
@@ -23,10 +23,59 @@ public class MarshmellowMonster
 		this.hasBloop = hasBloop;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyecount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
 	public String toString()
 	{
 		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only ";
-		description += tentacleAmount + " tentacles, but I have " + armCount + " arms!" + hasBloop + " tells you";
+		description += tentacleAmount + " tentacles, but I have " + armCount + " arms! " + hasBloop + " tells you";
 		description += " if I have a bloop";
 		return description;
 	}
